d97d38c9161ca47bc59d7251ab20eaf1f10692df
Antoine Cauquil <20204108+acauquil@users.noreply.github.com>
2018-03-12 16:27:17 +0100

    server: mongodb integration

--- server/server.js
+++ server/server.js
@@ -2,30 +2,33 @@ const compression = require("compression");
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-// const cookieParser = require("cookie-parser");
 const path = require("path");
 const auth = require("./auth");
-const http = require("http")
+const http = require("http");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8102;
 
 const app = express()
   .use(compression())
-//   .use(cookieParser())
   .use(morgan("tiny"))
   .use(bodyParser.json());
 
-// AUTHENTICATION
-app.use("/auth", auth);
-
-// UI
+// STATIC FILES
 app.use("/", express.static(path.join(__dirname, "/../dist")));
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve(path.join(__dirname, "/../dist/index.html")));
 });
 
+// MONGODB
+mongoose.connect("mongodb://localhost:27017/hivemind", () => {
+  console.log("connected to mongodb");
+});
+
+// AUTHENTICATION
+app.use("/auth", auth);
+
 const server = http.createServer(app);
 server.listen(PORT);
-// notifier.listen(server);
 
 console.log(`Server started at http://localhost:${PORT}`);
