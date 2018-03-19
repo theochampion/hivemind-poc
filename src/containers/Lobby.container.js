b77e98af187e110cb1e1b945c955e43251ffa256
Théo Champion <theo.champion@epitech.eu>
2018-03-19 14:35:16 +0100

    Lobby: prettify UI

--- src/containers/Lobby.container.js
+++ src/containers/Lobby.container.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import ReactPlayer from "react-player";
+import YouTubePlayer from "react-player/lib/players/YouTube";
+
 import { withStyles } from "material-ui/styles";
 
 import Card from "material-ui/Card";
@@ -23,14 +24,13 @@ const styles = theme => ({
   fontTitle: {
     position: "absolute",
     top: "40%",
-    left: "30%",
+    left: "10%",
     fontSize: "75px",
-    color: "white",
-    transform: "translate(-50%, -50%)"
+    color: "white"
   },
   fontDesc: {
     top: "60%",
-    left: "30%",
+    left: "18.5%",
     position: "absolute",
     fontSize: "50px",
     color: "white",
@@ -68,30 +68,30 @@ class Lobby extends Component {
     ));
     return (
       <div>
+        <div style={{ height: "100%", width: "100%", top: "-1em" }}>
+          <YouTubePlayer
+            youtubeConfig={{ playerVars: { showinfo: 1 } }}
+            url="https://www.youtube.com/watch?v=2s2dKBqpnIs"
+            height="1080px"
+            width="100%"
+            playing
+            loop
+            muted
+          />
+        </div>
+        <Typography className={classes.fontTitle}>Hivemind</Typography>
+        <Typography component="h1" className={classes.fontDesc}>
+          Make it together{" "}
+        </Typography>
         <div
           style={{
-            position: "relative",
-            textAlign: "center"
+            "margin-top": "-30em",
+            position: "absolute",
+            "background-color": "white"
           }}
         >
-          <div className="player-wrapper">
-            <ReactPlayer
-              className="react-player"
-              youtubeConfig={{ playerVars: { showinfo: 1 } }}
-              url="https://www.youtube.com/watch?v=2s2dKBqpnIs"
-              width="100%"
-              height="1100px"
-              playing
-              loop
-              muted
-            />
-          </div>
-          <h2 className={classes.fontTitle}>Do It Together</h2>
-          <text className={classes.fontDesc}>
-            Large-scale collaboration-enabled toolbox
-          </text>
+          <CardMatrix toolbar cards={cards} />
         </div>
-        <CardMatrix toolbar cards={cards} />
       </div>
     );
   }
