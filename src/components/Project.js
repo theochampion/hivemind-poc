f20625cfc5792738772bd92e4157620a8f6d845f
cheche <thomas.chevrie@epitech.eu>
2018-03-21 15:54:38 +0100

    Project: Add new feature to chat

--- src/components/Project.js
+++ src/components/Project.js
@@ -155,6 +155,7 @@ export const ProjectConversation = props => {
         id="name"
         label="Message"
         style={{ width: "100%", marginBottom: "1em" }}
+        onKeyDown={props.onNewMessage}
       />
     </div>
   );
@@ -207,7 +208,7 @@ export const ProjectContent = props => {
           <ProjectCards cards={props.cards} />
         </div>
         <div>
-          <ProjectConversation messages={props.messages} />
+          <ProjectConversation messages={props.messages} onNewMessage={props.onNewMessage}/>
         </div>
       </SwipeableViews>
     </div>
