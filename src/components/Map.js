a0467af933f5464261560ee32389167c699ea13b
Théo Champion <theo.champion@epitech.eu>
2018-03-21 15:23:59 +0100

    Map: Add fake cluster data

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import geodata from "../data/hives.json";
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoicmV0cm9ib3RpYyIsImEiOiJjamU4a2xoaG8wcHI2MnhtenJqaWxzd2dtIn0.xIBjrkrzz9_vpFeAvO37hw";
@@ -11,6 +12,68 @@ export default class Map extends React.Component {
     this.map = new mapboxgl.Map({
       container: this.mapContainer,
       style: "mapbox://styles/mapbox/streets-v9"
+    });
+
+    this.map.on("load", () => {
+      this.map.addSource("hives", {
+        type: "geojson",
+        data: geodata,
+        cluster: true,
+        clusterMaxZoom: 14, // Max zoom to cluster points on
+        clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+      });
+
+      this.map.addLayer({
+        id: "clusters",
+        type: "circle",
+        source: "hives",
+        filter: ["has", "point_count"],
+        paint: {
+          "circle-color": [
+            "step",
+            ["get", "point_count"],
+            "#51bbd6",
+            100,
+            "#f1f075",
+            750,
+            "#f28cb1"
+          ],
+          "circle-radius": [
+            "step",
+            ["get", "point_count"],
+            20,
+            100,
+            30,
+            750,
+            40
+          ]
+        }
+      });
+
+      this.map.addLayer({
+        id: "cluster-count",
+        type: "symbol",
+        source: "hives",
+        filter: ["has", "point_count"],
+        layout: {
+          "text-field": "{point_count_abbreviated}",
+          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+          "text-size": 12
+        }
+      });
+
+      this.map.addLayer({
+        id: "unclustered-point",
+        type: "circle",
+        source: "hives",
+        filter: ["!has", "point_count"],
+        paint: {
+          "circle-color": "#11b4da",
+          "circle-radius": 4,
+          "circle-stroke-width": 1,
+          "circle-stroke-color": "#fff"
+        }
+      });
     });
   }
 
